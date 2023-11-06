2af6779b0ab7f2dbc522f827d14d52b2bb783b47
Fahim Ahmed <94833665+fahime10@users.noreply.github.com>
2023-11-06 18:36:15 +0000

    Updated LinkedList.cpp

--- lab5/LinkedList.cpp
+++ lab5/LinkedList.cpp
@@ -8,26 +8,77 @@ void LinkedList::init() {
 }
 
 void LinkedList::cleanup() {
-	// IMPLEMENT ME
+	LNode* current_ = head_;
+
+    while (current_) {
+        LNode* temp = current_;
+        current_ = current_->next_;
+        delete temp;
+    }
+    cout << "Cleanup complete" << endl;
 }
 
 void LinkedList::print() const {
-	// IMPLEMENT ME
+	if (head_ == nullptr) {
+        return;
+    }
+
+    LNode* current_;
+    current_ = head_;
+
+    while (current_ != nullptr) {
+        cout << current_->data_ << endl;
+        current_ = current_->next_;
+    }
 }
 
 LNode* LinkedList::find(int t) const {
-	// IMPLEMENT ME
-	return nullptr; // stub
+	LNode* temp = head_;
+
+    while (temp != nullptr) {
+        if (temp->data_ == t) return temp;
+        temp = temp->next_;
+    }
+
+	return temp;
 }
 
 void LinkedList::insert_head(int t) {
-	// IMPLEMENT ME
+	auto temp = new LNode;
+    temp->data_ = t;
+    temp->next_ = head_;
+    head_ = temp;
 }
 
 void LinkedList::insert_after(LNode* location, int t) {
-	// IMPLEMENT ME
+	auto newNode = new LNode;
+    newNode->data_ = t;
+    newNode->next_ = location->next_;
+    location->next_ = newNode;
 }
 
 void LinkedList::remove(LNode* location) {
-	// IMPLEMENT ME
+	if (head_ == nullptr) {
+        return;
+    }
+
+    if (head_->data_ == location->data_) {
+        LNode* temp = head_;
+        head_ = head_->next_;
+        delete temp;
+        return;
+    }
+
+    LNode* current = head_;
+    LNode* prev = nullptr;
+
+    while (current != nullptr && current->data_ != location->data_) {
+        prev = current;
+        current = current->next_;
+    }
+
+    if (current != nullptr) {
+        prev->next_ = current->next_;
+        delete current;
+    }
 }
