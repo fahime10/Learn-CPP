7a09e963c026809c5d7ea080b658f2f37a0cf3e5
Fahim Ahmed <94833665+fahime10@users.noreply.github.com>
2023-11-06 18:36:35 +0000

    Updated main.cpp

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,23 +1,31 @@
 #include "LinkedList.h"
+#include <iostream>
+using namespace std;
 
-//int main() {
-//	LinkedList mylist;
-//	mylist.init();
-//	mylist.print();
-//	mylist.insert_head(1);
-//	mylist.insert_head(2);
-//	mylist.insert_head(3);
-//	mylist.print();
-//	LNode* result = mylist.find(3);
-//	if (result != nullptr) mylist.insert_after(result, 4);
-//	mylist.print();
-//	mylist.remove(mylist.find(3));
-//	mylist.print();
-//	mylist.remove(mylist.find(2));
-//	mylist.print();
-//	mylist.remove(mylist.find(4));
-//	mylist.print();
-//	mylist.remove(mylist.find(1));
-//	mylist.print();
-//	mylist.cleanup();
-//}
+int main() {
+	LinkedList mylist;
+	mylist.init();
+	mylist.print();
+	mylist.insert_head(1);
+	mylist.insert_head(2);
+	mylist.insert_head(3);
+	mylist.print();
+    cout << "\n" << endl;
+	LNode* result = mylist.find(3);
+    cout << "Result: " << result << "\n" << endl;
+	if (result != nullptr) mylist.insert_after(result, 4);
+	mylist.print();
+    cout << "\n" << endl;
+	mylist.remove(mylist.find(3));
+	mylist.print();
+    cout << "\n" << endl;
+	mylist.remove(mylist.find(2));
+	mylist.print();
+    cout << "\n" << endl;
+	mylist.remove(mylist.find(4));
+	mylist.print();
+    cout << "\n" << endl;
+	mylist.remove(mylist.find(1));
+	mylist.print();
+	mylist.cleanup();
+}
